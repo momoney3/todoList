05d5794aed61090a837b25764f7c9c75ebf75740
wilson <119863549+momoney3@users.noreply.github.com>
2025-09-10 22:41:58 +0100

    made a new storage.go file

---
+++ storage.go
@@ -0,0 +1,30 @@
+import (
+	"encoding/json"
+	"os"
+)
+
+type Storage[T any] struct {
+	Filesname string
+}
+
+func NewStorage[T any](filename string) *Storage[T] {
+	return &Storage[T]{Filesname: filename}
+}
+
+func (s *Storage[T]) Save(data T) error {
+	fileData, err := json.MarshalIndent(data, "", "   ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(s.Filename, fileData, 0o644)
+}
+
+func (s Storage[T]) Load(data *T) error {
+	fileData, err := os.ReadFile(s.Filesname)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(fileData, data)
+}
+
+
