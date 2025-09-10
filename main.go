5353bcdf27e887b487bde193cf2ac47a17cafb3c
wilson <119863549+momoney3@users.noreply.github.com>
2025-09-10 21:43:57 +0100

    created todos.json

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
+
+	"github.com/aquasecurity/table"
 )
 
 type Todo struct {
@@ -14,7 +17,7 @@ type Todo struct {
 
 type Todos []Todo
 
-func (t Todos) add(title string) {
+func (todos Todos) add(title string) {
 	todo := Todo{
 		Tile:      title,
 		Completed: false,
@@ -24,8 +27,53 @@ func (t Todos) add(title string) {
 	*todos = append(*todos, todo)
 }
 
-func (t Todos) delete(index int) error {
+func (todos Todos) delete(index int) error {
 	t := *Todos
+
+	if err := t.validateIndex(index); err != nil {
+		return
+	}
+	*todos = append(t[:index], t[index+1:]...)
+	return nil
+}
+
+func (todos *Todos) toggle(index int) error {
+	if err := todos.validateIndex(indix); err != nil {
+		return
+	}
+
+	t := *todos
+	todos := &t[indix]
+
+	if !todos.Completed {
+		completedTime := time.Now()
+		todos.CompletedAt = &completedTime
+	} else {
+		todo.CompletedAt = nil
+	}
+
+	todo.Completed = !todo.Completed
+	return nil
+}
+
+func (todos *Todos) print() {
+	table := table.New(os.Stdout)
+	table.SetRowlines(false)
+	table.setHeaders(("#", "Title", "Completed", "Created At", "Completed At")
+
+	for index, t :=  range *todos {
+		completed := "x"
+		completedAt := ""
+
+		if t.Completed {
+			completed = "✅"
+			if t.CompletedAt != nil {
+				completedAt = t.CompletedAt.Format(time.RFC123)
+			}
+		}
+		table.AddRow(strconv.IToa(index), t.Title, completed, t.CreatedAt)
+	}
+	table.Render()
 }
 
 func main() {
