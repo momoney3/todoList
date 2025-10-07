a1c16b1c9159a26d4a4d339cdd1f3b1dd52cb5d4
wilson <119863549+momoney3@users.noreply.github.com>
2025-10-07 21:03:26 +0100

    set up migration

--- magefile.go
+++ magefile.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/magefile/mage/sh"
 )
@@ -15,11 +16,9 @@ func Run() error {
 
 func Tidy() error {
 	fmt.Print("Tidy up ")
-
 	if err := sh.Run("go", "mod", "tidy"); err != nil {
 		return err
 	}
-
 	fmt.Println("and Cleaned")
 	return sh.Run("go", "clean")
 }
@@ -29,7 +28,33 @@ func Test() error {
 	return sh.Run("go", "test", "./...")
 }
 
-func Build() error {
-	fmt.Println("Building project")
-	return sh.Run("go", "build", "-o", "todo", "-v", "-a", "cmd/api/")
+func Build(tregger string) error {
+	switch tregger {
+	case "pm":
+		fmt.Println("Building image")
+		return sh.Run("podman", "build", "-t", "todolist-App", ".")
+
+	case "run":
+
+	case "run":
+		fmt.Println("Building Go project inside a Podman container")
+		return sh.Run(
+			"podman", "run", "--rm",
+			"-v", fmt.Sprintf("%s:/usr/src/myapp", os.Getenv("PWD")),
+			"-w", "/usr/src/myapp",
+			"golang:1.25",
+			"go", "build", "-v", "./cmd/api",
+		)
+
+	case "go":
+		fmt.Println("Building go project locally")
+		return sh.Run("go", "build", "-o", "todo", "-v", "-a", "./cmd/api")
+
+	case "print":
+		fmt.Println("it is working")
+		return sh.Run("echo", "it working")
+
+	default:
+		return fmt.Errorf("unknown fetcher: %s (expected: build, run, list)", tregger)
+	}
 }
